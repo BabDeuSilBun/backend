76b1501ccec4b0e1c1c081715eda871f667dfff1
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-05 16:43:40 +0900

    refactor: 조원 피드백 반영

--- src/main/java/com/zerobase/backend/enums/Bank.java
+++ src/main/java/com/zerobase/backend/enums/Bank.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum Bank {
+
   SHINHAN("신한"),
   NH("농협"),
   HANA("하나"),
@@ -11,13 +12,9 @@ public enum Bank {
   KAKAO("카카오뱅크"),
   TOSS("토스");
 
-  /*
-  신한, 농협, 하나, 국민, 카카오뱅크, 우리
-   */
+  private final String description;
 
-  private final String korean;
-
-  Bank(String korean) {
-    this.korean = korean;
+  Bank(String description) {
+    this.description = description;
   }
 }
