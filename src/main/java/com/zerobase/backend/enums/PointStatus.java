2a19b723090d48697142ee034868a6fefbe1bd81
hong <88184095+seokhyeong9171@users.noreply.github.com>
2024-08-06 11:18:44 +0900

    Update PointStatus.java

--- src/main/java/com/zerobase/backend/enums/PointStatus.java
+++ src/main/java/com/zerobase/backend/enums/PointStatus.java
@@ -1,8 +1,8 @@
 package com.zerobase.backend.enums;
 
 public enum PointStatus {
-    POINT_PLUS("포인트 증가"),
-    POINT_MINUS("포인트 감소");
+    PLUS("포인트 증가"),
+    MINUS("포인트 감소");
 
     private final String description;
 
