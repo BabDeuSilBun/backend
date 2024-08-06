9e6da0755c2b719e28509068ec4394433a2ee01f
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-06 09:12:56 +0900

    feature: 인증 관련 서비스 위한 request class 생성

--- src/main/java/com/zerobase/backend/security/controller/AuthController.java
+++ src/main/java/com/zerobase/backend/security/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.zerobase.backend.security.controller;
 
+import com.zerobase.backend.security.dto.SignRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +28,7 @@ public class AuthController {
    * 로그인
    */
   @PostMapping("/signin")
-  public ResponseEntity<?> signin() {
+  public ResponseEntity<?> signin(@Validated @RequestBody SignRequest.SignIn request) {
 
     return ResponseEntity.ok(null);
   }
