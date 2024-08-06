411b1b7d41c53b665db1c52ed793e526c98867e6
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-06 14:48:59 +0900

    feature: jwt component 생성

--- src/main/java/com/zerobase/backend/security/config/SecurityConfig.java
+++ src/main/java/com/zerobase/backend/security/config/SecurityConfig.java
@@ -11,8 +11,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -42,6 +46,18 @@ public class SecurityConfig {
     return http.build();
   }
 
+  /**
+   * 테스트용 계정
+   */
+//  @Bean
+  public UserDetailsService userDetailsService() {
+
+    UserDetails admin = User.withUsername("admin").password("{noop}1234").roles("ADMIN").build();
+    UserDetails user = User.withUsername("user").password("{noop}1234").roles("USER").build();
+    return new InMemoryUserDetailsManager(admin, user);
+  }
+
+  // cors 설정
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.addAllowedOrigin("http://localhost:3000");
