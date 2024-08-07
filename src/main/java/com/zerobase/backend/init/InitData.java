e90d2125eca4b9d9f4cb56c7ec4facb21a157826
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-07 15:46:24 +0900

    feature: 테스트용 초기 데이터 저장 기능 개발

---
+++ src/main/java/com/zerobase/backend/init/InitData.java
@@ -0,0 +1,39 @@
+package com.zerobase.backend.init;
+
+import com.zerobase.backend.domain.Major;
+import com.zerobase.backend.domain.School;
+import com.zerobase.backend.repository.MajorRepository;
+import com.zerobase.backend.repository.SchoolRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+@RequiredArgsConstructor
+public class InitData {
+
+  private final SchoolRepository schoolRepository;
+  private final MajorRepository majorRepository;
+
+  @Bean
+  public CommandLineRunner loadData() {
+
+    School schoolA = School.builder().name("schoolA").campus("campusA").build();
+    School schoolB = School.builder().name("schoolB").campus("campusB").build();
+    Major majorA = Major.builder().name("majorA").build();
+    Major majorB = Major.builder().name("majorB").build();
+
+    return args -> {
+      schoolRepository.save(schoolA);
+      schoolRepository.save(schoolB);
+      majorRepository.save(majorA);
+      majorRepository.save(majorB);
+    };
+  }
+
+
+
+}
