ce3985a075e207d2b6b6e05600cb8fa9638bb2cd
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-08 19:44:20 +0900

    refactor: 리팩토링

--- src/main/java/com/zerobase/backend/enums/MeetingStatus.java
+++ src/main/java/com/zerobase/backend/enums/MeetingStatus.java
@@ -6,29 +6,20 @@ import lombok.Getter;
 @Getter
 public enum MeetingStatus {
 
-  GATHERING(Status.PROGRESS, "모집중"),
-  ORDER_COMPLETED(Status.PROGRESS, "주문 완료"),
-  ORDER_CANCELLED(Status.PROGRESS, "주문 취소"),
-  COOKING(Status.PROGRESS, "조리중"),
-  COOKING_COMPLETED(Status.PROGRESS, "조리 완료"),
-  IN_DELIVERY(Status.PROGRESS, "배송중"),
-  DELIVERY_COMPLETED(Status.PROGRESS, "배송 완료"),
-  MEETING_CANCELLED(Status.CLOSED,"모임 취소"),
-  MEETING_COMPLETED(Status.CLOSED, "모임 완료");
+  GATHERING("모집중"),
+  ORDER_COMPLETED("주문 완료"),
+  ORDER_CANCELLED("주문 취소"),
+  COOKING("조리중"),
+  COOKING_COMPLETED("조리 완료"),
+  IN_DELIVERY("배송중"),
+  DELIVERY_COMPLETED("배송 완료"),
+  MEETING_CANCELLED("모임 취소"),
+  MEETING_COMPLETED("모임 완료");
 
-  private final Status status;
   private final String description;
 
-  private enum Status {
-    PROGRESS, CLOSED
-  }
-
-  MeetingStatus(Status status, String description) {
-    this.status = status;
+  MeetingStatus(String description) {
     this.description = description;
   }
 
-  public boolean isProgress() {
-    return this.getStatus() == Status.PROGRESS;
-  }
 }
