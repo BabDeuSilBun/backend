12c7d4ffaa8ea6d6e6a4ea0dee22c55898910a58
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-08 22:00:30 +0900

    feature: MeetingController 생성

---
+++ src/main/java/com/zerobase/babdeusilbun/meeting/controller/MeetingController.java
@@ -0,0 +1,25 @@
+package com.zerobase.babdeusilbun.meeting.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class MeetingController {
+
+
+  // 모임리스트 목록 조회/검색 api
+
+  // 주문 가능 가게 리스트 검색/조회 api
+
+  // 모임 정보 조회 api
+
+  // 모임 생성 api
+
+  // 가게 주문 전 모임 정보 수정 api
+
+  // 모임 탈퇴/취소 api
+
+}
