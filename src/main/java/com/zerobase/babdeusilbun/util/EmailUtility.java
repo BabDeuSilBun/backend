120f46ba25475064969f4b5ecbea816ceacdcc42
amj9843 <owo9@kakao.com>
2024-08-11 20:49:25 +0900

    feature: 이메일 전송 템플릿 생성

---
+++ src/main/java/com/zerobase/babdeusilbun/util/EmailUtility.java
@@ -0,0 +1,8 @@
+package com.zerobase.babdeusilbun.util;
+
+public class EmailUtility {
+  public final static String EMAIL_CODE_PREFIX = "email:code:";
+  public final static String EMAIL_COUNT_PREFIX = "email:count:";
+  public final static int EMAIL_CODE_EXPIRATION_MINUTES = 60;
+  public final static int EMAIL_VERIFY_MAX_COUNT = 5;
+}
