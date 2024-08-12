4f660d7138013634ef9325fe2c8e5b7bda219ec8
amj9843 <owo9@kakao.com>
2024-08-13 01:22:00 +0900

    test: 학교 검색 서비스 테스트

---
+++ src/test/java/com/zerobase/babdeusilbun/service/SchoolServiceTest.java
@@ -0,0 +1,45 @@
+package com.zerobase.babdeusilbun.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.zerobase.babdeusilbun.dto.SchoolDto.Information;
+import com.zerobase.babdeusilbun.repository.SchoolRepository;
+import com.zerobase.babdeusilbun.service.impl.SchoolServiceImpl;
+import java.util.Collections;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class SchoolServiceTest {
+  @Mock
+  private SchoolRepository schoolRepository;
+
+  @InjectMocks
+  private SchoolServiceImpl schoolService;
+
+  @DisplayName("학교 검색 서비스 테스트")
+  @Test
+  void searchSchoolAndCampus() {
+    //given
+    int page = 0;
+    int size = 10;
+    String search = "가짜";
+    String[] keywords = search.split(" +");
+    Page<Information> expectedPage = new PageImpl<>(Collections.emptyList());
+
+    when(schoolRepository.searchSchoolNameByKeywords(keywords, page, size)).thenReturn(expectedPage);
+
+    //when
+    Page<Information> result = schoolService.searchSchoolAndCampus(page, size, search);
+
+    //then
+    assertEquals(expectedPage, result);
+  }
+}
