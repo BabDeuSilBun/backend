2ea72d4c5007453e414ba15237132c5d34809537
amj9843 <owo9@kakao.com>
2024-08-13 13:52:21 +0900

    test: 로그인 정보 세팅, 캠퍼스 검색 컨트롤러 테스트 생성

--- src/test/java/com/zerobase/babdeusilbun/controller/SchoolControllerTest.java
+++ src/test/java/com/zerobase/babdeusilbun/controller/SchoolControllerTest.java
@@ -10,8 +10,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.zerobase.babdeusilbun.dto.SchoolDto.Information;
+import com.zerobase.babdeusilbun.security.dto.UserCustomUserDetails;
 import com.zerobase.babdeusilbun.service.SchoolService;
+import com.zerobase.babdeusilbun.util.TestUserUtility;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,6 +35,16 @@ class SchoolControllerTest {
 
   @MockBean
   private SchoolService schoolService;
+
+  @BeforeEach
+  void setUp() {
+    //로그인 정보 세팅
+    UserCustomUserDetails userDetails = TestUserUtility.createTestUser();
+
+    SecurityContextHolder.getContext().setAuthentication(
+        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities())
+    );
+  }
 
   @DisplayName("학교 검색 테스트")
   @WithMockUser
@@ -50,6 +65,58 @@ class SchoolControllerTest {
             .param("size", "10")
             .contentType(MediaType.APPLICATION_JSON)
             .with(csrf()))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.content").isArray())
+        .andExpect(jsonPath("$.content[0].name").value("가짜 학교"))
+        .andExpect(jsonPath("$.totalElements").value(1))
+        .andExpect(jsonPath("$.pageable").exists());
+  }
+
+  @DisplayName("캠퍼스 검색 테스트(schoolId에 값을 입력했을 경우)")
+  @Test
+  void searchCampusBySchoolWithSchoolId() throws Exception {
+    //given
+    Information info = new Information(2L, "가짜 학교", "가짜 캠퍼스");
+    Page<Information> expectedPage = new PageImpl<>(List.of(info));
+
+    //when
+    when(schoolService.searchCampusBySchool(2L, 0, 10))
+        .thenReturn(expectedPage);
+
+    //then
+    mockMvc.perform(get("/api/campus")
+            .param("schoolId", "2")
+            .param("page", "0")
+            .param("size", "10")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.content").isArray())
+        .andExpect(jsonPath("$.content[0].name").value("가짜 학교"))
+        .andExpect(jsonPath("$.totalElements").value(1))
+        .andExpect(jsonPath("$.pageable").exists());
+  }
+
+  @DisplayName("캠퍼스 검색 테스트(schoolId에 값이 없을 경우)")
+  @Test
+  void searchCampusBySchoolWithUserInfo() throws Exception {
+    //given
+    Information info = new Information(1L, "가짜 학교", "가짜 캠퍼스");
+    Page<Information> expectedPage = new PageImpl<>(List.of(info));
+
+    //when
+    when(schoolService.searchCampusBySchool(1L, 0, 10))
+        .thenReturn(expectedPage);
+
+    //then
+    mockMvc.perform(get("/api/campus")
+            .param("schoolId", "")
+            .param("page", "0")
+            .param("size", "10")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.content").isArray())
