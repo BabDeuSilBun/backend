384324e54ffc8207f3d498c77c0a83964a97dbee
amj9843 <owo9@kakao.com>
2024-08-15 01:38:03 +0900

    refactor: CustomUserDetails에서 변경에 따른 기존 동일 학교 다른 캠퍼스 조회 로직 수정

--- src/main/java/com/zerobase/babdeusilbun/service/SchoolService.java
+++ src/main/java/com/zerobase/babdeusilbun/service/SchoolService.java
@@ -1,10 +1,11 @@
 package com.zerobase.babdeusilbun.service;
 
 import com.zerobase.babdeusilbun.dto.SchoolDto.Information;
+import com.zerobase.babdeusilbun.security.dto.CustomUserDetails;
 import org.springframework.data.domain.Page;
 
 public interface SchoolService {
   Page<Information> searchSchoolAndCampus(String schoolName, int page, int size);
 
-  Page<Information> searchCampusBySchool(Long schoolId, int page, int size);
+  Page<Information> searchCampusBySchool(CustomUserDetails user, Long schoolId, int page, int size);
 }
