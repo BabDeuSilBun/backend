ed047ccf887f1472f01f61b628957b2811390009
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-14 22:27:17 +0900

    feat : 닉네임 무작위 생성 기능 구현

--- src/main/java/com/zerobase/babdeusilbun/security/service/impl/SignServiceImpl.java
+++ src/main/java/com/zerobase/babdeusilbun/security/service/impl/SignServiceImpl.java
@@ -217,9 +217,7 @@ public class SignServiceImpl implements SignService {
         .email(request.getEmail())
         .password(passwordEncoder.encode(request.getPassword()))
         .isBanned(false)
-        //TODO
-        // 닉네임 생성 유틸 필요
-        .nickname("testnickname")
+        .nickname(createNickname())
         .name(request.getName())
         .phoneNumber(request.getPhoneNumber())
         .point(0L)
@@ -271,5 +269,26 @@ public class SignServiceImpl implements SignService {
     if (!proceedingPurchase.isEmpty()) {
       throw new CustomException(ENTREPRENEUR_ORDER_PROCEEDING);
     }
+  }
+
+  private String createNickname() {
+    String[] modifiers = {
+            "배고픈", "굶주린", "예민한", "소심한", "대담한", "인기많은", "섬세한"
+    };
+    String[] nouns = {
+            "돼지", "병아리", "고양이", "강아지", "플라톤",
+            "아리스토텔레스", "소크라테스", "학생", "대학원생",
+            "예비대학원생", "탈출자", "기숙사생", "망령"
+    };
+
+    StringBuilder nickname = new StringBuilder("");
+
+    nickname.append(modifiers[(int) (Math.random() * modifiers.length)]);
+    nickname.append(nouns[(int) (Math.random() * nouns.length)]);
+    for(int i = 0; i < 3; i++) {
+      nickname.append((int) (Math.random() * 10));
+    }
+
+    return nickname.toString();
   }
 }
