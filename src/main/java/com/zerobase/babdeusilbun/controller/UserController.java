ee48033f2179079798408d1ef0d0be879e0cf3fe
amj9843 <owo9@kakao.com>
2024-08-15 02:20:16 +0900

    feature: User의 정보 변경 컨트롤러 생성

---
+++ src/main/java/com/zerobase/babdeusilbun/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.zerobase.babdeusilbun.controller;
+
+import static org.springframework.http.HttpStatus.PARTIAL_CONTENT;
+
+import com.zerobase.babdeusilbun.dto.UserDto;
+import com.zerobase.babdeusilbun.security.dto.CustomUserDetails;
+import com.zerobase.babdeusilbun.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+  private final UserService userService;
+
+  /**
+   * 내 정보 수정
+   */
+  @PreAuthorize("hasRole('USER')")
+  @PatchMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+  public ResponseEntity<?> updateProfile(
+      @AuthenticationPrincipal CustomUserDetails user,
+      @RequestPart(value = "file", required = false) MultipartFile image,
+      @RequestPart(value = "request") UserDto.UpdateRequest request) {
+    UserDto.UpdateRequest changeRequest = userService.updateProfile(user.getId(), image, request);
+
+    return (image != null && changeRequest.getImage() == null) ||
+        (request.getSchoolId() != null && changeRequest.getSchoolId() == null) ||
+        (request.getMajorId() != null && changeRequest.getMajorId() == null) ?
+        ResponseEntity.status(PARTIAL_CONTENT).build() : ResponseEntity.ok().build();
+  }
+}
