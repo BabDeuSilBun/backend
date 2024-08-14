b7348eaf95d4f71e65f0bfebd77a3593843e82bb
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-14 23:48:11 +0900

    refactor: CustomInquiryRepository 로 설정

--- src/main/java/com/zerobase/babdeusilbun/inquiry/service/impl/InquiryServiceImpl.java
+++ src/main/java/com/zerobase/babdeusilbun/inquiry/service/impl/InquiryServiceImpl.java
@@ -2,7 +2,7 @@ package com.zerobase.babdeusilbun.inquiry.service.impl;
 
 import com.zerobase.babdeusilbun.inquiry.dto.InquiryDto.ListResponse;
 import com.zerobase.babdeusilbun.inquiry.service.InquiryService;
-import com.zerobase.babdeusilbun.repository.custom.impl.CustomInquiryRepositoryImpl;
+import com.zerobase.babdeusilbun.repository.InquiryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,12 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InquiryServiceImpl implements InquiryService {
 
-  private final CustomInquiryRepositoryImpl customInquiryRepositoryImpl;
+  private final InquiryRepository inquiryRepository;
 
 
   @Override
   public Page<ListResponse> getInquiryList(String statusFilter, Pageable pageable) {
-    return customInquiryRepositoryImpl
+    return inquiryRepository
         .findInquiryList(statusFilter, pageable).map(ListResponse::fromEntity);
   }
 }
