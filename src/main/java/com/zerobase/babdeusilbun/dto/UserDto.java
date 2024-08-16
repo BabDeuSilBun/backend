1e226b7e4b8eb4170d37f7db4e45501646873cbf
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-17 02:19:55 +0900

    feat : 내 정보 조회시 반환할 값들을 저장할 인터페이스 구현

--- src/main/java/com/zerobase/babdeusilbun/dto/UserDto.java
+++ src/main/java/com/zerobase/babdeusilbun/dto/UserDto.java
@@ -1,8 +1,10 @@
 package com.zerobase.babdeusilbun.dto;
 
+import com.zerobase.babdeusilbun.domain.BankAccount;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.zerobase.babdeusilbun.domain.Address;
 
 public class UserDto {
   @Data
@@ -15,5 +17,25 @@ public class UserDto {
     private String phoneNumber;
     private Long schoolId;
     private Long majorId;
+  }
+
+  public interface MyPage {
+    String getName();
+    String getNickname();
+    String getPhoneNumber();
+    String getEmail();
+
+    Long getPoint();
+    String getImage();
+    Long getMeetingCount();
+    String getSchool();
+    String getCampus();
+    String getMajor();
+    Boolean getIsBanned();
+
+    Address getAddress();
+
+    BankAccount getBankAccount();
+
   }
 }
