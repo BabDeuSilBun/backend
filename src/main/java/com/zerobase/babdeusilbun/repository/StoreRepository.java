8fa0382dbfd85e5a8c075bf8370a46260ad370e8
amj9843 <owo9@kakao.com>
2024-08-16 14:34:30 +0900

    refactor: 상점 중복 확인을 위한 boolean 함수 생성

--- src/main/java/com/zerobase/babdeusilbun/repository/StoreRepository.java
+++ src/main/java/com/zerobase/babdeusilbun/repository/StoreRepository.java
@@ -1,7 +1,11 @@
 package com.zerobase.babdeusilbun.repository;
 
+import com.zerobase.babdeusilbun.domain.Address;
+import com.zerobase.babdeusilbun.domain.Entrepreneur;
 import com.zerobase.babdeusilbun.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+  boolean existsByEntrepreneurAndNameAndAddressAndDeletedAtIsNull(
+      Entrepreneur entrepreneur, String name, Address address);
 }
