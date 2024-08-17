c4c3931dc0e1c30dec92689f2f096d70d3cc2c64
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-17 12:09:46 +0900

    feature : 긍정평가배지, 부정평가배지 정보를 가져오는 기능 구현

--- src/main/java/com/zerobase/babdeusilbun/repository/EvaluateRepository.java
+++ src/main/java/com/zerobase/babdeusilbun/repository/EvaluateRepository.java
@@ -1,7 +1,34 @@
 package com.zerobase.babdeusilbun.repository;
 
 import com.zerobase.babdeusilbun.domain.Evaluate;
+import com.zerobase.babdeusilbun.dto.EvaluateDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EvaluateRepository extends JpaRepository<Evaluate, Long> {
+    @Query(value=
+            "select evaluate.content as content, ifnull(count(evaluate.content), 0) as count \n" +
+                    "from com.zerobase.babdeusilbun.domain.User as user, \n" +
+                    "com.zerobase.babdeusilbun.domain.Evaluate as evaluate \n" +
+                    "where evaluate.evaluateeId = user.id and user.email = :email and " +
+                    "(evaluate.content = com.zerobase.babdeusilbun.enums.EvaluateBadge.GOOD_COMMUNICATION or \n" +
+                    "evaluate.content = com.zerobase.babdeusilbun.enums.EvaluateBadge.GOOD_TIMECHECK or \n" +
+                    "evaluate.content = com.zerobase.babdeusilbun.enums.EvaluateBadge.GOOD_TOGETHER or \n" +
+                    "evaluate.content = com.zerobase.babdeusilbun.enums.EvaluateBadge.GOOD_RESPONSE) \n" +
+                    "group by content \n")
+    List<EvaluateDto.PositiveEvaluate> findPositiveEvaluatesByEmail(String email);
+
+    @Query(value=
+            "select evaluate.content as content, ifnull(count(evaluate.content), 0) as count \n" +
+                    "from com.zerobase.babdeusilbun.domain.User as user, \n" +
+                    "com.zerobase.babdeusilbun.domain.Evaluate as evaluate \n" +
+                    "where evaluate.evaluateeId = user.id and user.email = :email and " +
+                    "(evaluate.content = com.zerobase.babdeusilbun.enums.EvaluateBadge.BAD_RESPONSE or \n" +
+                    "evaluate.content = com.zerobase.babdeusilbun.enums.EvaluateBadge.BAD_TIMECHECK or \n" +
+                    "evaluate.content = com.zerobase.babdeusilbun.enums.EvaluateBadge.BAD_TOGETHER) \n" +
+                    "group by content \n")
+    List<EvaluateDto.NegativeEvaluate> findNegativeEvaluatesByEmail(String email);
+
 }
