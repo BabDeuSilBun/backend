04d7f863ffe1592c4c003781f08cc405d21b816d
amj9843 <owo9@kakao.com>
2024-08-18 03:25:31 +0900

    refactor: 상점 생성 기능 이미지 업로드와 분리, 상점에 배달가능캠퍼스 등록,제외 생성

--- src/main/java/com/zerobase/babdeusilbun/service/StoreService.java
+++ src/main/java/com/zerobase/babdeusilbun/service/StoreService.java
@@ -2,14 +2,19 @@ package com.zerobase.babdeusilbun.service;
 
 import com.zerobase.babdeusilbun.dto.CategoryDto;
 import com.zerobase.babdeusilbun.dto.CategoryDto.Information;
+import com.zerobase.babdeusilbun.dto.SchoolDto;
+import com.zerobase.babdeusilbun.dto.StoreDto;
 import com.zerobase.babdeusilbun.dto.StoreDto.CreateRequest;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StoreService {
-  int createStore(Long entrepreneurId, List<MultipartFile> images, CreateRequest request);
+  StoreDto.IdResponse createStore(Long entrepreneurId, CreateRequest request);
+  int uploadImageToStore(Long entrepreneurId, List<MultipartFile> images, Long storeId);
   Page<Information> getAllCategories(int page, int size);
   int enrollToCategory(Long entrepreneurId, Long storeId, CategoryDto.IdsRequest request);
   int deleteOnCategory(Long entrepreneurId, Long storeId, CategoryDto.IdsRequest request);
+  int enrollSchoolsToStore(Long entrepreneurId, Long storeId, SchoolDto.IdsRequest request);
+  int deleteSchoolsOnStore(Long entrepreneurId, Long storeId, SchoolDto.IdsRequest request);
 }
--- src/main/java/com/zerobase/babdeusilbun/service/impl/StoreServiceImpl.java
+++ src/main/java/com/zerobase/babdeusilbun/service/impl/StoreServiceImpl.java
@@ -9,12 +9,16 @@ import static com.zerobase.babdeusilbun.util.ImageUtility.STORE_IMAGE_FOLDER;
 import com.zerobase.babdeusilbun.component.ImageComponent;
 import com.zerobase.babdeusilbun.domain.Category;
 import com.zerobase.babdeusilbun.domain.Entrepreneur;
+import com.zerobase.babdeusilbun.domain.School;
 import com.zerobase.babdeusilbun.domain.Store;
 import com.zerobase.babdeusilbun.domain.StoreCategory;
 import com.zerobase.babdeusilbun.domain.StoreImage;
+import com.zerobase.babdeusilbun.domain.StoreSchool;
 import com.zerobase.babdeusilbun.dto.CategoryDto.IdsRequest;
 import com.zerobase.babdeusilbun.dto.CategoryDto.Information;
+import com.zerobase.babdeusilbun.dto.SchoolDto;
 import com.zerobase.babdeusilbun.dto.StoreDto.CreateRequest;
+import com.zerobase.babdeusilbun.dto.StoreDto.IdResponse;
 import com.zerobase.babdeusilbun.dto.StoreDto.ImageUrl;
 import com.zerobase.babdeusilbun.exception.CustomException;
 import com.zerobase.babdeusilbun.repository.CategoryRepository;
@@ -23,6 +27,7 @@ import com.zerobase.babdeusilbun.repository.SchoolRepository;
 import com.zerobase.babdeusilbun.repository.StoreCategoryRepository;
 import com.zerobase.babdeusilbun.repository.StoreImageRepository;
 import com.zerobase.babdeusilbun.repository.StoreRepository;
+import com.zerobase.babdeusilbun.repository.StoreSchoolRepository;
 import com.zerobase.babdeusilbun.service.StoreService;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,13 +48,14 @@ public class StoreServiceImpl implements StoreService {
   private final StoreRepository storeRepository;
   private final StoreImageRepository imageRepository;
   private final SchoolRepository schoolRepository;
+  private final StoreSchoolRepository storeSchoolRepository;
   private final CategoryRepository categoryRepository;
   private final StoreCategoryRepository storeCategoryRepository;
   private final ImageComponent imageComponent;
 
   @Override
   @Transactional
-  public int createStore(Long entrepreneurId, List<MultipartFile> images, CreateRequest request) {
+  public IdResponse createStore(Long entrepreneurId, CreateRequest request) {
     Entrepreneur entrepreneur = entrepreneurRepository
         .findByIdAndDeletedAtIsNull(entrepreneurId).orElseThrow(() -> new CustomException(ENTREPRENEUR_NOT_FOUND));
 
@@ -59,21 +65,36 @@ public class StoreServiceImpl implements StoreService {
       throw new CustomException(ALREADY_EXIST_STORE);
     }
 
-    Store store = request.toEntity(entrepreneur);
-    storeRepository.save(store);
+    Store store = storeRepository.save(request.toEntity(entrepreneur));
+
+    return IdResponse.builder().storeId(store.getId()).build();
+  }
+
+  @Override
+  @Transactional
+  public int uploadImageToStore(Long entrepreneurId, List<MultipartFile> images, Long storeId) {
+    Entrepreneur entrepreneur = entrepreneurRepository
+        .findByIdAndDeletedAtIsNull(entrepreneurId).orElseThrow(() -> new CustomException(ENTREPRENEUR_NOT_FOUND));
+
+    Store store = storeRepository
+        .findByIdAndDeletedAtIsNull(storeId).orElseThrow(() -> new CustomException(STORE_NOT_FOUND));
+
+    if (store.getEntrepreneur() != entrepreneur) {
+      throw new CustomException(NO_AUTH_ON_STORE);
+    }
 
     List<StoreImage> uploadImageList = new ArrayList<>();
-    AtomicInteger sequence = new AtomicInteger();
+    AtomicInteger sequence = new AtomicInteger(imageRepository.countByStore(store));
 
     imageComponent.uploadImageList(images, STORE_IMAGE_FOLDER).forEach(url ->
-      uploadImageList.add(
-          ImageUrl.builder()
-              .url(url)
-              .isRepresentative(sequence.get() == 0)
-              .sequence(sequence.getAndIncrement())
-              .build()
-              .toEntity(store)
-      )
+        uploadImageList.add(
+            ImageUrl.builder()
+                .url(url)
+                .isRepresentative(sequence.get() == 0)
+                .sequence(sequence.getAndIncrement())
+                .build()
+                .toEntity(store)
+        )
     );
 
     if (!uploadImageList.isEmpty()) {
@@ -145,5 +166,63 @@ public class StoreServiceImpl implements StoreService {
     }
 
     return storeCategoryRepository.deleteByStoreAndCategory_IdIn(store, request.getCategoryIds());
+  }
+
+  @Override
+  @Transactional
+  public int enrollSchoolsToStore(Long entrepreneurId, Long storeId, SchoolDto.IdsRequest request) {
+    Entrepreneur entrepreneur = entrepreneurRepository
+        .findByIdAndDeletedAtIsNull(entrepreneurId).orElseThrow(() -> new CustomException(ENTREPRENEUR_NOT_FOUND));
+
+    Store store = storeRepository
+        .findByIdAndDeletedAtIsNull(storeId).orElseThrow(() -> new CustomException(STORE_NOT_FOUND));
+
+    if (store.getEntrepreneur() != entrepreneur) {
+      throw new CustomException(NO_AUTH_ON_STORE);
+    }
+
+    List<Long> curSchoolIds = storeSchoolRepository.findSchoolIdsByStore(store);
+    AtomicInteger count = new AtomicInteger();
+
+    request.getSchoolIds().forEach(id -> {
+      if (curSchoolIds.contains(id)) {
+        log.info("this school is already enrolled on this store."
+            + " schoolId -> {}, storeId -> {}", id, store.getId());
+
+        return;
+      }
+      Optional<School> school = schoolRepository.findById(id);
+
+      //카테고리로 조회된 게 없는 경우
+      if (school.isEmpty()) {
+        log.error("failed to enroll school on store cause there's no school found by id."
+            + " schoolId -> {}, storeId -> {} ", id, store.getId());
+      } else {
+        storeSchoolRepository.save(StoreSchool.builder()
+            .store(store)
+            .school(school.get())
+            .build());
+
+        count.getAndIncrement();
+      }
+    });
+
+    return count.get();
+  }
+
+  @Override
+  @Transactional
+  public int deleteSchoolsOnStore(Long entrepreneurId, Long storeId, SchoolDto.IdsRequest request) {
+    Entrepreneur entrepreneur = entrepreneurRepository
+        .findByIdAndDeletedAtIsNull(entrepreneurId).orElseThrow(() -> new CustomException(ENTREPRENEUR_NOT_FOUND));
+
+    Store store = storeRepository
+        .findByIdAndDeletedAtIsNull(storeId).orElseThrow(() -> new CustomException(STORE_NOT_FOUND));
+
+    if (store.getEntrepreneur() != entrepreneur) {
+      throw new CustomException(NO_AUTH_ON_STORE);
+    }
+
+    return storeSchoolRepository.deleteByStoreAndSchool_IdIn(store, request.getSchoolIds());
   }
 }
