20be5da774d2e8c8044a1eac7104c99e1bcca1e1
amj9843 <owo9@kakao.com>
2024-08-18 03:24:46 +0900

    refactor: service에서 사용되는 함수들 추가, repository 어노테이션 명시

--- src/main/java/com/zerobase/babdeusilbun/repository/StoreImageRepository.java
+++ src/main/java/com/zerobase/babdeusilbun/repository/StoreImageRepository.java
@@ -4,9 +4,12 @@ import com.zerobase.babdeusilbun.domain.Store;
 import com.zerobase.babdeusilbun.domain.StoreImage;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StoreImageRepository extends JpaRepository<StoreImage, Long> {
 
   List<StoreImage> findAllByStoreOrderBySequenceAsc(Store store);
+  int countByStore(Store store);
 
 }
--- src/main/java/com/zerobase/babdeusilbun/repository/StoreSchoolRepository.java
+++ src/main/java/com/zerobase/babdeusilbun/repository/StoreSchoolRepository.java
@@ -1,7 +1,18 @@
 package com.zerobase.babdeusilbun.repository;
 
+import com.zerobase.babdeusilbun.domain.Store;
 import com.zerobase.babdeusilbun.domain.StoreSchool;
+import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StoreSchoolRepository extends JpaRepository<StoreSchool, Long> {
+  @Query("SELECT ss.school.id FROM StoreSchool ss WHERE ss.store = :store")
+  List<Long> findSchoolIdsByStore(@Param("store") Store store);
+
+  int deleteByStoreAndSchool_IdIn(Store store, Set<Long> schoolIds);
 }
