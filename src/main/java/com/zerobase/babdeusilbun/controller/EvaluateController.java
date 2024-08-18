8222637551fb9575d540cad014357c29aa3c49a6
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-18 16:57:35 +0900

    fix : 기존의 메서드에 로그인한 사용자 정보를 저장할 매개변수 추가

--- src/main/java/com/zerobase/babdeusilbun/controller/EvaluateController.java
+++ src/main/java/com/zerobase/babdeusilbun/controller/EvaluateController.java
@@ -1,10 +1,12 @@
 package com.zerobase.babdeusilbun.controller;
 
 import com.zerobase.babdeusilbun.dto.EvaluateDto;
+import com.zerobase.babdeusilbun.security.dto.CustomUserDetails;
 import com.zerobase.babdeusilbun.service.EvaluateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,8 @@ public class EvaluateController {
 
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/evaluates")
-    public ResponseEntity<EvaluateDto.MyEvaluates> getMyEvaluates() {
-        EvaluateDto.MyEvaluates myEvaluates = evaluateService.getEvaluates();
+    public ResponseEntity<EvaluateDto.MyEvaluates> getMyEvaluates(@AuthenticationPrincipal CustomUserDetails user) {
+        EvaluateDto.MyEvaluates myEvaluates = evaluateService.getEvaluates(user.getId());
         return ResponseEntity.ok(myEvaluates);
     }
 }
