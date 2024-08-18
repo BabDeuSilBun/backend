940dad2c3be07ce7f5e8d7f27dd3f71f74e30cdf
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-19 00:18:40 +0900

    feat : 메뉴 생성 시 필요한 ErrorCode 추가 fix : NO_AUTH_ON_STORE 메세지 수정

--- src/main/java/com/zerobase/babdeusilbun/exception/ErrorCode.java
+++ src/main/java/com/zerobase/babdeusilbun/exception/ErrorCode.java
@@ -68,7 +68,10 @@ public enum ErrorCode {
   // 상점 관련
   STORE_NOT_FOUND(NOT_FOUND, "couldn't find store"),
   ALREADY_EXIST_STORE(CONFLICT, "already have store which user want to enroll."),
-  NO_AUTH_ON_STORE(FORBIDDEN, "no auth to modify or delete this storage"),
+  NO_AUTH_ON_STORE(FORBIDDEN, "no auth to use or modify or delete this storage"),
+
+  // 메뉴 관련
+  ALREADY_EXIST_MENU(CONFLICT, "already have menu which user want to enroll."),
 
   //이메일 인증 관련
   CANNOT_SEND_MAIL_EXCEEDS_MAX_COUNT(BAD_REQUEST,
