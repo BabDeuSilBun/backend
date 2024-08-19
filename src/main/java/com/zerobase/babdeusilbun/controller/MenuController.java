4ac6f0971cb4817bc300e61b6284b9dee964c27e
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-19 18:25:57 +0900

    feat : 메뉴 수정, 삭제 컨트롤러 구현

--- src/main/java/com/zerobase/babdeusilbun/controller/MenuController.java
+++ src/main/java/com/zerobase/babdeusilbun/controller/MenuController.java
@@ -1,5 +1,6 @@
 package com.zerobase.babdeusilbun.controller;
 
+import com.zerobase.babdeusilbun.domain.Menu;
 import com.zerobase.babdeusilbun.dto.MenuDto;
 import com.zerobase.babdeusilbun.security.dto.CustomUserDetails;
 import com.zerobase.babdeusilbun.service.MenuService;
@@ -10,8 +11,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.PARTIAL_CONTENT;
@@ -30,8 +29,7 @@ public class MenuController {
     @PostMapping(value = "/businesses/stores/{storeId}/menus",
             consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Void> createMenu(
-            @AuthenticationPrincipal
-    CustomUserDetails entrepreneur,
+            @AuthenticationPrincipal CustomUserDetails entrepreneur,
             @PathVariable("storeId") Long storeId,
             @RequestPart(value = "file", required = false) MultipartFile image,
             @RequestPart(value = "request") MenuDto.CreateRequest request) {
@@ -42,4 +40,35 @@ public class MenuController {
                 ResponseEntity.status(PARTIAL_CONTENT).build() : ResponseEntity.status(CREATED).build();
     }
 
+    /**
+     * 메뉴 수정
+     */
+    @PreAuthorize("hasRole('ENTREPRENEUR')")
+    @PatchMapping(value = "/businesses/menus/{menuId}",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Void> updateMenu(
+            @AuthenticationPrincipal CustomUserDetails entrepreneur,
+            @PathVariable("menuId") Long menuId,
+            @RequestPart(value = "file", required = false) MultipartFile image,
+            @RequestPart(value = "request") MenuDto.UpdateRequest request
+    ) {
+
+        MenuDto.UpdateRequest result = menuService.updateMenu(entrepreneur.getId(), menuId, image, request);
+
+        return (image != null && result.getImage() == null) ?
+                ResponseEntity.status(PARTIAL_CONTENT).build() : ResponseEntity.ok().build();
+    }
+
+    /**
+     * 메뉴 삭제
+     */
+    @PreAuthorize("hasRole('ENTREPRENEUR')")
+    @DeleteMapping(value = "/businesses/menus/{menuId}")
+    public ResponseEntity<Void> deleteMmenu(
+            @AuthenticationPrincipal CustomUserDetails entrepreneur,
+            @PathVariable("menuId") Long menuId) {
+
+        menuService.deleteMenu(entrepreneur.getId(), menuId);
+        return ResponseEntity.ok().build();
+    }
 }
