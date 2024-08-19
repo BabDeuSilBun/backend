9fda79924feba5fe65e7a8a356bee39e3a253cbc
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-19 21:45:12 +0900

    feature: entity graph 적용

--- src/main/java/com/zerobase/babdeusilbun/repository/IndividualPurchaseRepository.java
+++ src/main/java/com/zerobase/babdeusilbun/repository/IndividualPurchaseRepository.java
@@ -4,9 +4,11 @@ import com.zerobase.babdeusilbun.domain.IndividualPurchase;
 import com.zerobase.babdeusilbun.domain.Purchase;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IndividualPurchaseRepository extends JpaRepository<IndividualPurchase, Long> {
 
+  @EntityGraph(attributePaths = {"purchase", "menu"})
   Page<IndividualPurchase> findAllByPurchase(Purchase purchase, Pageable pageable);
 }
