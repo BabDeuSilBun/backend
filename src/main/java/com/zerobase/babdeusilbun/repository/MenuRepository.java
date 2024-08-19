a054958ee8b8002676613450d3bb66f3dab3bc87
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-19 18:24:53 +0900

    feat : 식별번호를 기반으로 메뉴를 조회하는 기능 추가

--- src/main/java/com/zerobase/babdeusilbun/repository/MenuRepository.java
+++ src/main/java/com/zerobase/babdeusilbun/repository/MenuRepository.java
@@ -4,6 +4,10 @@ import com.zerobase.babdeusilbun.domain.Menu;
 import com.zerobase.babdeusilbun.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    Optional<Menu> findByIdAndDeletedAtIsNull(Long menuId);
+
     boolean existsByStoreAndNameAndPriceAndDeletedAtIsNull(Store store, String name, long price);
 }
