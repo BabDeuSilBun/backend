3c82bbbb4bef6c034c706d7310740a28d366871e
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-19 18:23:06 +0900

    fix : Menu 클래스의 EqualsAndHashCode 어노테이션에 (callSuper=false) 추가

--- src/main/java/com/zerobase/babdeusilbun/domain/Menu.java
+++ src/main/java/com/zerobase/babdeusilbun/domain/Menu.java
@@ -1,6 +1,7 @@
 package com.zerobase.babdeusilbun.domain;
 
 
+import com.zerobase.babdeusilbun.dto.MenuDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -23,7 +24,7 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor
 @Builder
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper=false)
 @Table(name = "menu",
     // 가게에서 같은 메뉴(메뉴 이름과 가격이 동일)는 등록하지 못함
     uniqueConstraints = @UniqueConstraint(columnNames = {"store_id", "name", "price"})
@@ -51,4 +52,14 @@ public class Menu extends BaseEntity{
 
   private LocalDateTime deletedAt;
 
+  public void update(MenuDto.UpdateRequest request) {
+    if(request.getName() != null) this.name = request.getName();
+    if(request.getDescription() != null) this.description = request.getDescription();
+    if(request.getImage() != null) this.image = request.getImage();
+    if(request.getPrice() >= 0) this.price = request.getPrice();
+  }
+
+  public void delete() {
+    deletedAt = LocalDateTime.now();
+  }
 }
