12e75d1fd8c7b0510ed602a9a01a6e056c5f5b9c
amj9843 <owo9@kakao.com>
2024-08-20 23:54:06 +0900

    refactor: Repository 명시

--- src/main/java/com/zerobase/babdeusilbun/repository/StoreRepository.java
+++ src/main/java/com/zerobase/babdeusilbun/repository/StoreRepository.java
@@ -5,9 +5,10 @@ import com.zerobase.babdeusilbun.domain.Entrepreneur;
 import com.zerobase.babdeusilbun.domain.Store;
 import com.zerobase.babdeusilbun.repository.custom.CustomStoreRepository;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StoreRepository extends JpaRepository<Store, Long>, CustomStoreRepository {
   boolean existsByEntrepreneurAndNameAndAddressAndDeletedAtIsNull(
       Entrepreneur entrepreneur, String name, Address address);
