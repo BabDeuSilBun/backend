4be7a1e2e12dea77664d0cf58daa3d6b584a2a10
rrr7777777goo <rrr7777777goo@gmail.com>
2024-08-22 01:22:30 +0900

    feat : Menu와 Purchase를 기반으로 IndividualPurchase가 존재하는지 확인하는 쿼리 생성

--- src/main/java/com/zerobase/babdeusilbun/repository/IndividualPurchaseRepository.java
+++ src/main/java/com/zerobase/babdeusilbun/repository/IndividualPurchaseRepository.java
@@ -1,6 +1,7 @@
 package com.zerobase.babdeusilbun.repository;
 
 import com.zerobase.babdeusilbun.domain.IndividualPurchase;
+import com.zerobase.babdeusilbun.domain.Menu;
 import com.zerobase.babdeusilbun.domain.Purchase;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,4 +12,6 @@ public interface IndividualPurchaseRepository extends JpaRepository<IndividualPu
 
   @EntityGraph(attributePaths = {"purchase", "menu"})
   Page<IndividualPurchase> findAllByPurchase(Purchase purchase, Pageable pageable);
+
+  boolean existsAllByMenuAndPurchase(Menu menu, Purchase purchase);
 }
