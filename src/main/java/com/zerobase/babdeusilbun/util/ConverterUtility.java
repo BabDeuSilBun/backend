f7c351631fe0cc8cf1fbb197970d7dd58c6e1953
amj9843 <owo9@kakao.com>
2024-08-21 12:32:57 +0900

    feature: DayOfWeek 객체 한국어로 변경하는 컨버터 유틸리티 생성

---
+++ src/main/java/com/zerobase/babdeusilbun/util/ConverterUtility.java
@@ -0,0 +1,21 @@
+package com.zerobase.babdeusilbun.util;
+
+import java.time.DayOfWeek;
+
+public class ConverterUtility {
+  public static String dayOfWeekConvert(DayOfWeek dayOfWeek) {
+    if (dayOfWeek == null) {
+      throw new IllegalArgumentException("DayOfWeek cannot be null");
+    }
+
+    return switch (dayOfWeek) {
+      case MONDAY -> "월요일";
+      case TUESDAY -> "화요일";
+      case WEDNESDAY -> "수요일";
+      case THURSDAY -> "목요일";
+      case FRIDAY -> "금요일";
+      case SATURDAY -> "토요일";
+      case SUNDAY -> "일요일";
+    };
+  }
+}
