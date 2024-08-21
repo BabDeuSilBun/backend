f60594af3c4f1b5c5e0077df55c9b299788a5664
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-21 16:47:12 +0900

    refactor: @Transactional(readOnly = true) 추가

--- src/main/java/com/zerobase/babdeusilbun/service/impl/SnapshotServiceImpl.java
+++ src/main/java/com/zerobase/babdeusilbun/service/impl/SnapshotServiceImpl.java
@@ -31,8 +31,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SnapshotServiceImpl implements SnapshotService {
 
