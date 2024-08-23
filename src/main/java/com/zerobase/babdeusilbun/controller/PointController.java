31ad815406375674c2466aa00473136f3cfab403
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-23 15:07:11 +0900

    feature: 기본 컨트롤러 생성

---
+++ src/main/java/com/zerobase/babdeusilbun/controller/PointController.java
@@ -0,0 +1,12 @@
+package com.zerobase.babdeusilbun.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class PointController {
+
+}
---
+++ src/main/java/com/zerobase/babdeusilbun/service/impl/PointServiceImpl.java
@@ -0,0 +1,11 @@
+package com.zerobase.babdeusilbun.service.impl;
+
+import com.zerobase.babdeusilbun.service.PointService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointServiceImpl implements PointService {
+
+}
