d7aa028346c466d0a3d966865d89d8a982392cca
amj9843 <owo9@kakao.com>
2024-08-28 02:39:35 +0900

    refactor: 모임별 주문 승인, 거절, 조리완료, 지연사유 작성 서비스 구현

--- src/main/java/com/zerobase/babdeusilbun/service/impl/MeetingServiceImpl.java
+++ src/main/java/com/zerobase/babdeusilbun/service/impl/MeetingServiceImpl.java
@@ -2,40 +2,77 @@ package com.zerobase.babdeusilbun.service.impl;
 
 import static com.zerobase.babdeusilbun.dto.MeetingRequest.Create;
 import static com.zerobase.babdeusilbun.enums.MeetingStatus.GATHERING;
+import static com.zerobase.babdeusilbun.enums.PointType.PLUS;
 import static com.zerobase.babdeusilbun.enums.PurchaseStatus.PRE_PURCHASE;
+import static com.zerobase.babdeusilbun.enums.UserAlarmType.COOKING_COMPLETED;
+import static com.zerobase.babdeusilbun.enums.UserAlarmType.ORDER_APPROVED;
+import static com.zerobase.babdeusilbun.enums.UserAlarmType.ORDER_DELAY;
+import static com.zerobase.babdeusilbun.enums.UserAlarmType.ORDER_REJECTED;
+import static com.zerobase.babdeusilbun.enums.UserAlarmType.POINT_REFUND;
 import static com.zerobase.babdeusilbun.exception.ErrorCode.CHATROOM_NOT_FOUND;
+import static com.zerobase.babdeusilbun.exception.ErrorCode.ENTREPRENEUR_NOT_FOUND;
 import static com.zerobase.babdeusilbun.exception.ErrorCode.MEETING_LEADER_NOT_MATCH;
 import static com.zerobase.babdeusilbun.exception.ErrorCode.MEETING_NOT_FOUND;
 import static com.zerobase.babdeusilbun.exception.ErrorCode.MEETING_PARTICIPANT_EXIST;
+import static com.zerobase.babdeusilbun.exception.ErrorCode.MEETING_PURCHASE_TIME_NOT_FOUND;
 import static com.zerobase.babdeusilbun.exception.ErrorCode.MEETING_STATUS_INVALID;
+import static com.zerobase.babdeusilbun.exception.ErrorCode.NO_AUTH_ON_PURCHASE;
+import static com.zerobase.babdeusilbun.exception.ErrorCode.NO_AUTH_ON_STORE;
 import static com.zerobase.babdeusilbun.exception.ErrorCode.PURCHASE_NOT_FOUND;
+import static com.zerobase.babdeusilbun.exception.ErrorCode.PURCHASE_PAYMENT_NOT_FOUND;
 import static com.zerobase.babdeusilbun.exception.ErrorCode.STORE_NOT_FOUND;
 import static com.zerobase.babdeusilbun.exception.ErrorCode.USER_NOT_FOUND;
+import static com.zerobase.babdeusilbun.util.MeetingUtility.CANCELED_STATUS;
+import static com.zerobase.babdeusilbun.util.MeetingUtility.CAN_ENTREPRENEUR_CHECK_PURCHASE_STATUS;
+import static com.zerobase.babdeusilbun.util.MeetingUtility.ENTREPRENEUR_CAN_COMPLETE;
+import static com.zerobase.babdeusilbun.util.MeetingUtility.ENTREPRENEUR_CAN_CONFIRM_OR_DENY;
+import static com.zerobase.babdeusilbun.util.MeetingUtility.ENTREPRENEUR_CAN_SEND_DELAY_MESSAGE;
+import static com.zerobase.babdeusilbun.util.MeetingUtility.getTitle;
 
 import com.zerobase.babdeusilbun.domain.ChatRoom;
+import com.zerobase.babdeusilbun.domain.Entrepreneur;
 import com.zerobase.babdeusilbun.domain.Meeting;
+import com.zerobase.babdeusilbun.domain.MeetingPurchaseTime;
+import com.zerobase.babdeusilbun.domain.Point;
 import com.zerobase.babdeusilbun.domain.Purchase;
+import com.zerobase.babdeusilbun.domain.PurchasePayment;
 import com.zerobase.babdeusilbun.domain.Store;
 import com.zerobase.babdeusilbun.domain.StoreImage;
 import com.zerobase.babdeusilbun.domain.User;
+import com.zerobase.babdeusilbun.domain.UserAlarm;
+import com.zerobase.babdeusilbun.dto.ChatDto.Request;
 import com.zerobase.babdeusilbun.dto.DeliveryAddressDto;
 import com.zerobase.babdeusilbun.dto.MeetingDto;
 import com.zerobase.babdeusilbun.dto.MeetingRequest.Update;
 import com.zerobase.babdeusilbun.dto.MetAddressDto;
+import com.zerobase.babdeusilbun.dto.PurchaseDto.MenuResponse;
 import com.zerobase.babdeusilbun.dto.StoreImageDto;
+import com.zerobase.babdeusilbun.enums.PurchaseStatus;
+import com.zerobase.babdeusilbun.enums.UserAlarmType;
 import com.zerobase.babdeusilbun.exception.CustomException;
 import com.zerobase.babdeusilbun.repository.ChatRoomRepository;
+import com.zerobase.babdeusilbun.repository.EntrepreneurRepository;
+import com.zerobase.babdeusilbun.repository.MeetingPurchaseTimeRepository;
 import com.zerobase.babdeusilbun.repository.MeetingRepository;
+import com.zerobase.babdeusilbun.repository.PointRepository;
+import com.zerobase.babdeusilbun.repository.PurchasePaymentRepository;
 import com.zerobase.babdeusilbun.repository.PurchaseRepository;
 import com.zerobase.babdeusilbun.repository.StoreImageRepository;
 import com.zerobase.babdeusilbun.repository.StoreRepository;
+import com.zerobase.babdeusilbun.repository.UserAlarmRepository;
 import com.zerobase.babdeusilbun.repository.UserRepository;
 import com.zerobase.babdeusilbun.scheduler.MeetingScheduler;
 import com.zerobase.babdeusilbun.security.dto.CustomUserDetails;
 import com.zerobase.babdeusilbun.service.MeetingService;
+import io.micrometer.common.util.StringUtils;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -46,12 +83,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class MeetingServiceImpl implements MeetingService {
 
   private final MeetingRepository meetingRepository;
+  private final MeetingPurchaseTimeRepository meetingPurchaseTimeRepository;
   private final StoreImageRepository storeImageRepository;
   private final UserRepository userRepository;
+  private final EntrepreneurRepository entrepreneurRepository;
   private final StoreRepository storeRepository;
   private final PurchaseRepository purchaseRepository;
+  private final PurchasePaymentRepository purchasePaymentRepository;
   private final MeetingScheduler meetingScheduler;
   private final ChatRoomRepository chatRoomRepository;
+  private final PointRepository pointRepository;
+  private final UserAlarmRepository userAlarmRepository;
 
   private final ChatServiceImpl chatService;
 
@@ -180,6 +222,151 @@ public class MeetingServiceImpl implements MeetingService {
     return userRepository.countMeetingParticipant(meetingId).intValue();
   }
 
+  @Override
+  @Transactional
+  public void confirmMeetingPurchase(Long entrepreneurId, Long meetingId) {
+    Meeting meeting = meetingRepository
+        .findByIdAndStatusInAndDeletedAtIsNull(meetingId, ENTREPRENEUR_CAN_CONFIRM_OR_DENY)
+        .orElseThrow(() -> new CustomException(MEETING_NOT_FOUND));
+
+    confirmEntrepreneurAuthOfPurchaseByMeeting(
+        entrepreneurRepository.findByIdAndDeletedAtIsNull(entrepreneurId)
+        .orElseThrow(() -> new CustomException(ENTREPRENEUR_NOT_FOUND)), meeting);
+
+    MeetingPurchaseTime meetingPurchaseTime = meetingPurchaseTimeRepository.findByMeeting(meeting)
+        .orElseGet(() -> createMeetingPurchaseTimeForMeeting(meeting, meeting.getStore()));
+
+    //모임상태 변경
+    meeting.confirmMeetingPurchase();
+    
+    //totalAmount 만큼 결제(밥드실분 -> 상점) : 진행되었다 가정
+    Long totalAmount = getTotalPurchaseAmountOfMeeting(
+        purchaseRepository.findAllByMeetingAndStatus(meeting, PurchaseStatus.PAYMENT_COMPLETED)
+    );
+
+    //시간 기록
+    meetingPurchaseTime.writeProcessedAt();
+  }
+
+  @Override
+  @Transactional
+  public void denyMeetingPurchase(Long entrepreneurId, Long meetingId) {
+    Meeting meeting = meetingRepository
+        .findByIdAndStatusInAndDeletedAtIsNull(meetingId, ENTREPRENEUR_CAN_CONFIRM_OR_DENY)
+        .orElseThrow(() -> new CustomException(MEETING_NOT_FOUND));
+
+    confirmEntrepreneurAuthOfPurchaseByMeeting(
+        entrepreneurRepository.findByIdAndDeletedAtIsNull(entrepreneurId)
+            .orElseThrow(() -> new CustomException(ENTREPRENEUR_NOT_FOUND)), meeting);
+
+    MeetingPurchaseTime meetingPurchaseTime = meetingPurchaseTimeRepository.findByMeeting(meeting)
+        .orElseGet(() -> createMeetingPurchaseTimeForMeeting(meeting, meeting.getStore()));
+
+    //모임상태 변경
+    meeting.denyMeetingPurchase();
+
+    //모임에 개인 주문 내역(주문 상태: 결제 완료) 확인
+    purchaseRepository.findAllByMeetingAndStatus(meeting, PurchaseStatus.PAYMENT_COMPLETED)
+        .forEach(purchase -> {
+          PurchasePayment purchasePayment =
+              purchasePaymentRepository.findByMeetingAndUser(purchase.getMeeting(), purchase.getUser())
+                  .orElseThrow(() -> new CustomException(PURCHASE_PAYMENT_NOT_FOUND));
+
+          //차액 환급
+          Long refundPoint = getTotalIndividualPaymentAmount(purchasePayment);
+          refundPointToUser(purchase.getUser(), purchasePayment, refundPoint);
+
+          //알림 전송(멤버별 주문이 거절되었어요/ㅇㅇ포인트가 환급되었어요)
+          userAlarmRepository.saveAll(List.of(
+              purchaseStatusAlarm(purchase, ORDER_REJECTED), refundAlarmOfPurchase(purchase, refundPoint)
+          ));
+
+          //모임, 이용자 식별번호에 따른 주문 상태 모두 취소로 변경
+          purchase.cancel();
+        });
+
+    //시간 기록
+    meetingPurchaseTime.writeProcessedAt();
+  }
+
+  @Override
+  @Transactional
+  public void completeMeetingPurchase(Long entrepreneurId, Long meetingId) {
+    Meeting meeting = meetingRepository
+        .findByIdAndStatusInAndDeletedAtIsNull(meetingId, ENTREPRENEUR_CAN_COMPLETE)
+        .orElseThrow(() -> new CustomException(MEETING_NOT_FOUND));
+
+    confirmEntrepreneurAuthOfPurchaseByMeeting(
+        entrepreneurRepository.findByIdAndDeletedAtIsNull(entrepreneurId)
+            .orElseThrow(() -> new CustomException(ENTREPRENEUR_NOT_FOUND)), meeting);
+
+    MeetingPurchaseTime meetingPurchaseTime = meetingPurchaseTimeRepository.findByMeeting(meeting)
+        .orElseThrow(() -> new CustomException(MEETING_PURCHASE_TIME_NOT_FOUND));
+
+    //모임상태 변경
+    meeting.completedCooking();
+
+    //모임에 개인 주문 내역(주문 상태: 결제 완료) 확인 후 알람 전송
+    List<UserAlarm> alarms = purchaseRepository.findAllByMeetingAndStatus(meeting, PurchaseStatus.PAYMENT_COMPLETED)
+        .stream().map(this::completedCookAlarmOfMeeting).toList();
+    userAlarmRepository.saveAll(alarms);
+
+    //시간 기록
+    meetingPurchaseTime.writeCookedAt();
+  }
+
+  @Override
+  @Transactional
+  public void sendMessageForDelayMeetingPurchases(Long entrepreneurId, Long meetingId, Request request) {
+    Meeting meeting = meetingRepository
+        .findByIdAndStatusInAndDeletedAtIsNull(meetingId, ENTREPRENEUR_CAN_SEND_DELAY_MESSAGE)
+        .orElseThrow(() -> new CustomException(MEETING_NOT_FOUND));
+
+    confirmEntrepreneurAuthOfPurchaseByMeeting(
+        entrepreneurRepository.findByIdAndDeletedAtIsNull(entrepreneurId)
+            .orElseThrow(() -> new CustomException(ENTREPRENEUR_NOT_FOUND)), meeting);
+
+    //모임에 개인 주문 내역(주문 상태: 결제 완료) 확인, 알람 메세지 전송(조리/배차가 지연되고 있어요.)
+    List<UserAlarm> alarms = purchaseRepository.findAllByMeetingAndStatus(meeting, PurchaseStatus.PAYMENT_COMPLETED)
+        .stream().map(purchase -> delayOfMeeting(purchase, request.getContent())).toList();
+    userAlarmRepository.saveAll(alarms);
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public Page<MenuResponse> getMeetingPurchaseByStoreIdAndMeetingId(Long entrepreneurId, Long storeId, Long meetingId,
+      int page, int size) {
+    Meeting meeting = meetingRepository.findById(meetingId)
+        .orElseThrow(() -> new CustomException(MEETING_NOT_FOUND));
+
+    if (!Objects.equals(meeting.getStore().getId(), storeId)
+        || !Objects.equals(meeting.getStore().getEntrepreneur().getId(), entrepreneurId)) {
+      throw new CustomException(NO_AUTH_ON_STORE);
+    }
+
+    if (CAN_ENTREPRENEUR_CHECK_PURCHASE_STATUS.contains(meeting.getStatus())) {
+      return getMenuByMeetingAndStatus(meeting, page, size);
+    } else {
+      throw new CustomException(NO_AUTH_ON_PURCHASE); //주문 내역을 볼 수 있는 권한이 있는 상태가 아님
+    }
+  }
+
+  public Page<MenuResponse> getMenuByMeetingAndStatus(Meeting meeting, int page, int size) {
+    PurchaseStatus status = (CANCELED_STATUS.contains(meeting.getStatus())) ?
+        PurchaseStatus.CANCEL : PurchaseStatus.PAYMENT_COMPLETED;
+
+    int count = meetingRepository.countPurchaseMenuByMeetingAndStatus(meeting.getId(), status.name());
+    if (count == 0) {
+      return new PageImpl<>(new ArrayList<>(), PageRequest.of(0, 1), 0);
+    }
+
+    size = (size <= 0) ? count : size;
+    page = Math.min(page, ((int) Math.ceil((double) count / size))-1);
+
+    Pageable pageable = PageRequest.of(page, size);
+
+    return meetingRepository.findAllPurchaseMenuByMeetingAndStatus(meeting.getId(), status.name(), pageable);
+  }
 
   private void verifyExistParticipant(Meeting findMeeting) {
     if (purchaseRepository.findAllByMeeting(findMeeting).size() != 1) {
@@ -263,4 +450,106 @@ public class MeetingServiceImpl implements MeetingService {
         .orElseThrow(() -> new CustomException(CHATROOM_NOT_FOUND));
   }
 
+  private void confirmEntrepreneurAuthOfPurchaseByMeeting(Entrepreneur entrepreneur, Meeting meeting) {
+    if (Objects.equals(entrepreneur, meeting.getStore().getEntrepreneur())) {
+      throw new CustomException(NO_AUTH_ON_PURCHASE);
+    }
+  }
+
+  private MeetingPurchaseTime createMeetingPurchaseTimeForMeeting(Meeting meeting, Store store) {
+    return meetingPurchaseTimeRepository.save(
+        MeetingPurchaseTime.builder()
+            .meeting(meeting)
+            .store(store)
+            .build()
+    );
+  }
+
+  private Long getRefundAmountWhenConfirmByPurchasePayment(PurchasePayment purchasePayment, int headCount) {
+    return purchasePayment.getDeliveryFee() + purchasePayment.getTeamPurchaseFee()
+        - (purchasePayment.getDeliveryPrice() + purchasePayment.getTeamPurchasePrice())/headCount;
+  }
+
+  private Long getTotalIndividualPaymentAmount(PurchasePayment purchasePayment) {
+    return purchasePayment.getIndividualPurchasePrice()
+        + purchasePayment.getDeliveryFee()
+        + purchasePayment.getTeamPurchaseFee();
+  }
+
+  private void refundPointToUser(User user, PurchasePayment purchasePayment, Long point) {
+    user.plusPoint(point);
+
+    pointRepository.save(
+        Point.builder()
+            .user(user).purchasePayment(purchasePayment)
+            .type(PLUS).amount(point)
+            .content(PLUS.getContent())
+            .build()
+    );
+  }
+
+  private UserAlarm purchaseStatusAlarm(Purchase purchase, UserAlarmType type) {
+    return UserAlarm.builder()
+        .user(purchase.getUser())
+        .type(type)
+        .content(type.getMessage(getTitle(purchase.getMeeting())))
+        .build();
+  }
+
+  private UserAlarm refundAlarmOfPurchase(Purchase purchase, Long refundPoint) {
+    return UserAlarm.builder()
+        .user(purchase.getUser())
+        .type(POINT_REFUND)
+        .content(POINT_REFUND.getMessage(getTitle(purchase.getMeeting()), refundPoint.toString()))
+        .build();
+  }
+
+  private UserAlarm completedCookAlarmOfMeeting(Purchase purchase) {
+    return UserAlarm.builder()
+        .user(purchase.getUser())
+        .type(COOKING_COMPLETED)
+        .content(COOKING_COMPLETED.getMessage(getTitle(purchase.getMeeting())))
+        .build();
+  }
+
+  private UserAlarm delayOfMeeting(Purchase purchase, String plusContent) {
+    if (StringUtils.isBlank(plusContent)) {
+      plusContent = "개인 사정";
+    }
+
+    return UserAlarm.builder()
+        .user(purchase.getUser())
+        .type(ORDER_DELAY)
+        .content(ORDER_DELAY.getMessage(getTitle(purchase.getMeeting()), plusContent))
+        .build();
+  }
+
+  private Long getTotalPurchaseAmountOfMeeting(List<Purchase> purchases) {
+    //모임에서 가게로 결제해야 하는 금액
+    AtomicLong totalAmount = new AtomicLong(0);
+
+    //모임에 주문 내역(주문 상태: 결제 완료) 확인 > 참여자 별로 주문 승인 과정 시행
+    purchases.forEach(purchase -> {
+      PurchasePayment purchasePayment =
+          purchasePaymentRepository.findByMeetingAndUser(purchase.getMeeting(), purchase.getUser())
+              .orElseThrow(() -> new CustomException(PURCHASE_PAYMENT_NOT_FOUND));
+
+      //차액 환급
+      Long refundPoint = getRefundAmountWhenConfirmByPurchasePayment(purchasePayment, purchases.size());
+      refundPointToUser(purchase.getUser(), purchasePayment, refundPoint);
+
+      //알림 전송(멤버별 주문이 승인되었어요/ㅇㅇ포인트가 환급되었어요)
+      userAlarmRepository.saveAll(List.of(
+          purchaseStatusAlarm(purchase, ORDER_APPROVED), refundAlarmOfPurchase(purchase, refundPoint)
+      ));
+
+      if (totalAmount.get() == 0) {
+        totalAmount.getAndAdd(purchasePayment.getDeliveryPrice() + purchasePayment.getTeamPurchasePrice());
+      }
+
+      totalAmount.getAndAdd(purchasePayment.getIndividualPurchasePrice());
+    });
+
+    return totalAmount.get();
+  }
 }
