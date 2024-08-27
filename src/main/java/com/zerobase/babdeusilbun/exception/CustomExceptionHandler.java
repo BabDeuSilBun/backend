09862e64b2e0c7afdef4a9814da87ac8adf735f2
SeokHyeong9171 <hsh9171@icloud.com>
2024-08-28 00:17:35 +0900

    refactor: SecurityConfig 수정

--- src/main/java/com/zerobase/babdeusilbun/exception/CustomExceptionHandler.java
+++ src/main/java/com/zerobase/babdeusilbun/exception/CustomExceptionHandler.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +22,18 @@ public class CustomExceptionHandler {
 
     return ResponseEntity
         .status(e.getErrorCode().getStatus())
+        .body(ErrorResponse.of(e));
+  }
+
+  @ExceptionHandler(BindException.class)
+  public ResponseEntity<ErrorResponse> handleBindException
+      (HttpServletRequest request, BindException e) {
+
+    log.error("=== ERROR!!! ===");
+    log.error(request.getRequestURI());
+
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
         .body(ErrorResponse.of(e));
   }
 
