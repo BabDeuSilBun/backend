90cd2b3b546f2206d42cf44515994bd2b2870ea8
amj9843 <owo9@kakao.com>
2024-09-01 01:57:52 +0900

    refactor: 인터셉터 추가, Principal 객체를 CustomUserDetails로 바로 변환해주는 resolveArgument 추가

--- src/main/java/com/zerobase/babdeusilbun/config/WebSocketConfig.java
+++ src/main/java/com/zerobase/babdeusilbun/config/WebSocketConfig.java
@@ -4,9 +4,23 @@ import static com.zerobase.babdeusilbun.util.ChatUtility.SEND_TO_CLIENT_PREFIX;
 import static com.zerobase.babdeusilbun.util.ChatUtility.SEND_TO_SERVER_PREFIX;
 import static com.zerobase.babdeusilbun.util.ChatUtility.STOMP_PREFIX;
 
+import com.zerobase.babdeusilbun.component.JwtChannelInterceptor;
+import com.zerobase.babdeusilbun.exception.CustomException;
+import com.zerobase.babdeusilbun.exception.ErrorCode;
+import com.zerobase.babdeusilbun.security.dto.CustomUserDetails;
+import jakarta.annotation.Nullable;
+import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -15,6 +29,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+  private final JwtChannelInterceptor jwtChannelInterceptor;
+
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
     config.enableSimpleBroker(SEND_TO_CLIENT_PREFIX);
@@ -23,6 +39,40 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint(STOMP_PREFIX).setAllowedOrigins("*");//.withSockJS();
+    registry.addEndpoint(STOMP_PREFIX).setAllowedOriginPatterns("*");
+  }
+
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(jwtChannelInterceptor);
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+    argumentResolvers.add(customUserDetailsWebsocketArgumentResolver());
+  }
+
+  @Bean
+  HandlerMethodArgumentResolver customUserDetailsWebsocketArgumentResolver() {
+    return new CustomUserDetailsWebsocketArgumentResolver();
+  }
+
+  static class CustomUserDetailsWebsocketArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+      return CustomUserDetails.class.isAssignableFrom(parameter.getParameterType());
+    }
+
+    @Override
+    @Nullable
+    public Object resolveArgument(MethodParameter parameter, Message<?> message) {
+      Principal principal = SimpMessageHeaderAccessor.getUser(message.getHeaders());
+
+      if (principal instanceof AbstractAuthenticationToken abstractAuthenticationToken) {
+        return abstractAuthenticationToken.getPrincipal();
+      }
+
+      throw new CustomException(ErrorCode.USER_NOT_FOUND);
+    }
   }
 }
