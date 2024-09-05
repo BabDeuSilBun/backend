caee66b3eede11c980ff4cd1e0c705be6bd4cb15
SeokHyeong9171 <hsh9171@icloud.com>
2024-09-05 12:17:14 +0900

    refactor: time zone 변경

--- src/main/java/com/zerobase/babdeusilbun/config/JpaConfig.java
+++ src/main/java/com/zerobase/babdeusilbun/config/JpaConfig.java
@@ -2,7 +2,9 @@ package com.zerobase.babdeusilbun.config;
 
 import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
+import java.util.TimeZone;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -13,5 +15,11 @@ public class JpaConfig {
   @Bean
   public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
     return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+  }
+
+  @PostConstruct
+  public void started() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    System.out.println("현재 시스템 시간대: " + TimeZone.getDefault().getID());
   }
 }
